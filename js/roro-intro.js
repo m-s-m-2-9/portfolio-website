e183968f61c444863da8ae27e82cff829464ed57
Manomay Shailendra Misra <msmgamingcool29@gmail.com>
2026-05-22 14:06:57 +0530

    Center align element by adjusting margin and position

--- js/roro-intro.js
+++ js/roro-intro.js
@@ -199,7 +199,8 @@
         transparent 0%, rgba(255,255,255,0.55) 15%,
         rgba(255,255,255,0.8) 50%, rgba(255,255,255,0.55) 85%, transparent 100%);
       transform-origin: center;
-      margin-left: -2.2vw;
+      left: 50%;
+transform: translateX(-50%);
     }
     #rs-name {
       font-family: 'Cinzel', Georgia, serif;
