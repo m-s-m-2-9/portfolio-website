54f257e120734698c61b3509a89e1a5dd4d0005a
Manomay Shailendra Misra <msmgamingcool29@gmail.com>
2026-05-28 19:22:08 +0530

    Update social.js

--- admin-control/pages/social.js
+++ admin-control/pages/social.js
@@ -30,7 +30,7 @@ window.ADMIN_SOCIAL = {
       text  : '"Manomay did a fantastic job designing the logo for our PG. He took the time to truly understand the vibe and target audience of our accommodation. The entire process was incredibly smooth, and he translated our vision into a creative, memorable logo that perfectly represents our brand. Highly recommend his design services!"',
       author: '— Radha Madankar, Owner @ Golden Star PG',
     },
-    {d
+    {
       stars : 5,
       text  : '"Manomay successfully integrated our website’s chatbot automation, delivering human-like, 24/7 customer support. Their technical expertise and smart workflows reduced our support workload and streamlined lead qualification. Highly recommended for anyone wanting to elevate their digital presence!"',
       author: '— Ramaiah SK, Manager @ Mayura Woods',
